9c0055da4d7fbca4f305cd5bfc739ef315b2c983
Zakaria Belarbi <zakariabelarbi02@gmail.com>
2025-01-20 19:29:45 +0100

    Read the all body content and fix some parssing features

--- request/requestLine/RequestLine.cpp
+++ request/requestLine/RequestLine.cpp
@@ -6,17 +6,27 @@
 /*   By: zaelarb <zaelarb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 13:12:25 by zaelarb           #+#    #+#             */
-/*   Updated: 2025/01/16 14:57:00 by zaelarb          ###   ########.fr       */
+/*   Updated: 2025/01/20 17:22:53 by zaelarb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RequestLine.hpp"
 
 RequestLine::RequestLine(const std::string &requestLine) {
-    this->method = strtok((char *)requestLine.c_str(), " ");
-    this->requestTarget = strtok(NULL, " ");
-    this->requestTarget = strtok(NULL, " ");
+    // this->method = strtok((char *)requestLine.c_str(), " ");
+    // this->requestTarget = strtok(NULL, " ");
+    // this->requestTarget = strtok(NULL, " ");
     // this->httpVersion = requestLine;
+    int index = requestLine.find('\n');
+    std::string line = requestLine.substr(0, index);
+
+    index = line.find(' ');
+    this->method = line.substr(0, index);
+    line.erase(0, index + 1);
+    index = line.find(' ');
+    this->requestTarget = line.substr(0, index);
+    line.erase(0, index + 1);
+    this->httpVersion = line;
 
     std::cout << "Method : " << this->method << std::endl;
     std::cout << "Request Target : " << this->requestTarget << std::endl;
